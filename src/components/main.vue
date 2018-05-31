fc21b989d33c3f1c08583e9d481ec12b7e7cae68
Anu1601CS <anuragvns1111@gmail.com>
2018-06-01 01:59:49 +0530

    UI

---
+++ src/components/main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{ filename }}</h1>
+    <h2>{{ file() }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      filename: 'Main'
+    }
+  },
+  methods:{
+    file: function(){
+      return 'Anurag Kumar';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+/style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
+// Import Components
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import VueResource from 'vue-resource'
+import Main from './components/main.vue'
+import Content from './components/content.vue'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+// Register Components
+Vue.component('anurag', Main)
+Vue.component('media-content', Content)
+
+// Use Components
+Vue.use(VueResource)
+Vue.use(Vuetify)
+
+// Create the root Vue instance
+document.addEventListener("DOMContentLoaded",
+    (e) => new Vue({
+        el: '#media',
+        render: h => h(App)
+    })
+)
